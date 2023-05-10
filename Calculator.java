c000d7b7d6b5857d4ae87b7877dc679fc77c1f2f
Luis Arocutipa <larocutipa@unsa.edu.pe>
2023-05-09 22:14:23 -0500

    Se agregó unos casos de ejemplo para probar los métodos

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,11 @@
 public class Calculator {
 
     public static void main(String[]args){
-
+        System.out.println("La suma de 3 y 9 es:" + add(3,9));
+        System.out.println("La resta de 9 y 3 es:" + sub(9,3));
+        System.out.println("La multiplicación de 3 y 9 es:" + mul(3,9));
+        System.out.println("La división de 9 y 3 es:" + div(9,3));
+        System.out.println("8 en módulo 3 es:" + mod(8,3));
     }
 
     public static int add(int num1, int num2){
